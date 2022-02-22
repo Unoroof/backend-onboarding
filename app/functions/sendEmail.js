9b85bdc8c0493fdbfc4f4ac53c7b8f5261dfff4c
rajeshn95 <rajesh@betalectic.com>
2022-02-22 19:23:44 +0530

    changes in payload for help with partner and product for email

--- app/functions/sendEmail.js
+++ app/functions/sendEmail.js
@@ -8,36 +8,86 @@ module.exports = async (enquiry, profile) => {
         event_type: "user_raised_partner_enquiry",
         user_id: enquiry.user_uuid,
         data: {
-          name: profile.name,
-          // "currency": "INR",
-          // "domestic": "yes",
-          // "exporter": "Yes",
-          // "importer": "No",
-          // "lc_limit": "Yes",
-          // "supplier": "Yes",
-          // "turnover": "25-100",
-          // "borrowing": "",
-          // "cin_number": "1234567890",
-          // "sblc_limit": "Yes",
-          // "description": "help asap",
-          // "loan_amount": "100000000",
-          // "company_name": "betalectic",
-          // "buyer_segment": "",
-          // "credit_amount": "1000000",
-          // "borrowing_cost": "10",
-          // "current_amount": "20000000",
-          // "credit_currency": "USD",
-          // "current_currency": "INR",
-          // "max_interest_rate": "70%",
-          // "credit_rating_long": "10",
-          // "credit_funded_limit": "20",
-          // "credit_rating_short": "8",
-          // "current_funded_limit": "30",
-          // "current_product_used": "Buyer credit",
-          // "credit_non_funded_limit": "80",
-          // "current_non_funded_limit": "70",
-          // "current_financial_partner": "HDFC",
-          // "supplier_bill_discounting": "No"
+          name: profile.data.full_name ? profile.data.full_name : "-",
+          company_name: enquiry.data.company_name
+            ? enquiry.data.company_name
+            : "-",
+          cin_number: enquiry.data.cin ? enquiry.data.cin : "-",
+          turnover: enquiry.data.range ? enquiry.data.range : "-",
+          buyer_segment: enquiry.data.buyer_segment
+            ? enquiry.data.buyer_segment
+            : "-",
+          product_name: enquiry.data.current_products
+            ? enquiry.data.current_products
+            : "-",
+          currency: enquiry.data.currency_type.value
+            ? enquiry.data.currency_type.value
+            : "-",
+          loan_amount: enquiry.data.financing_amount
+            ? enquiry.data.financing_amount
+            : enquiry.data.financing_amount,
+          max_interest_rate_inr: enquiry.data.inr_cost
+            ? enquiry.data.inr_cost
+            : "-", // %
+          max_interest_rate_usd: enquiry.data.usd_cost
+            ? enquiry.data.usd_cost
+            : "-", // %
+          description: enquiry.data.requirements
+            ? enquiry.data.requirements
+            : "-",
+          exporter: enquiry.data.exporter ? enquiry.data.exporter : "No",
+          importer: enquiry.data.importer ? enquiry.data.importer : "No",
+          domestic: enquiry.data.domestic ? enquiry.data.domestic : "No",
+          borrowing: enquiry.data.borrowing_type
+            ? enquiry.data.borrowing_type
+            : "Unsecured",
+          borrowing_cost: enquiry.data.collateral
+            ? enquiry.data.collateral
+            : "-",
+          credit_rating_long:
+            enquiry.data.credit_rating_long_term.length > 0
+              ? enquiry.data.credit_rating_long_term[0].rating
+              : "-",
+          credit_rating_short:
+            enquiry.data.credit_rating_short_term.length > 0
+              ? enquiry.data.credit_rating_short_term[0].rating
+              : "-",
+          current_financial_partner:
+            enquiry.data.financial_Patners.length > 0
+              ? enquiry.data.financial_Patners
+              : "-",
+          sblc_limit: enquiry.data.sblc_limit ? enquiry.data.sblc_limit : "No", // yes/no
+          lc_limit: enquiry.data.lc_limit ? enquiry.data.lc_limit : "No", // yes/no
+          supplier: enquiry.data.large_cap_supplier
+            ? enquiry.data.large_cap_supplier
+            : "No", // yes/no
+          supplier_bill_discounting: enquiry.data.supplier_discounting
+            ? enquiry.data.supplier_discounting
+            : "No", // yes/no
+          credit_amount: enquiry.data.total_current_credit_volume
+            ? enquiry.data.total_current_credit_volume
+            : "-",
+          credit_currency: enquiry.data.total_current_credit_currency
+            ? enquiry.data.total_current_credit_currency
+            : "-",
+          credit_funded_limit: enquiry.data.funded_credit
+            ? enquiry.data.funded_credit
+            : "-",
+          credit_non_funded_limit: enquiry.data.non_funded_credit
+            ? enquiry.data.non_funded_credit
+            : "-",
+          current_amount: enquiry.data.current_utilization_volume
+            ? enquiry.data.current_utilization_volume
+            : "-",
+          current_currency: enquiry.data.current_utilization_currency
+            ? enquiry.data.current_utilization_currency
+            : "-",
+          current_funded_limit: enquiry.data.funded_utilization
+            ? enquiry.data.funded_utilization
+            : "-",
+          current_non_funded_limit: enquiry.data.non_funded_utilization
+            ? enquiry.data.non_funded_utilization
+            : "-",
         },
         ignore_user_contacts: true,
         contact_infos: [
@@ -62,8 +112,86 @@ module.exports = async (enquiry, profile) => {
         event_type: "user_raised_product_enquiry",
         user_id: enquiry.user_uuid,
         data: {
-          name: "Rajesh", // enquiry.data.name,
-          company_name: "Betalectic", // enquiry.data.company_name,
+          name: profile.data.full_name ? profile.data.full_name : "-",
+          company_name: enquiry.data.company_name
+            ? enquiry.data.company_name
+            : "-",
+          cin_number: enquiry.data.cin ? enquiry.data.cin : "-",
+          turnover: enquiry.data.range ? enquiry.data.range : "-",
+          buyer_segment: enquiry.data.buyer_segment
+            ? enquiry.data.buyer_segment
+            : "-",
+          product_name: enquiry.data.current_products
+            ? enquiry.data.current_products
+            : "-",
+          currency: enquiry.data.currency_type.value
+            ? enquiry.data.currency_type.value
+            : "-",
+          loan_amount: enquiry.data.financing_amount
+            ? enquiry.data.financing_amount
+            : enquiry.data.financing_amount,
+          max_interest_rate_inr: enquiry.data.inr_cost
+            ? enquiry.data.inr_cost
+            : "-", // %
+          max_interest_rate_usd: enquiry.data.usd_cost
+            ? enquiry.data.usd_cost
+            : "-", // %
+          description: enquiry.data.requirements
+            ? enquiry.data.requirements
+            : "-",
+          exporter: enquiry.data.exporter ? enquiry.data.exporter : "No",
+          importer: enquiry.data.importer ? enquiry.data.importer : "No",
+          domestic: enquiry.data.domestic ? enquiry.data.domestic : "No",
+          borrowing: enquiry.data.borrowing_type
+            ? enquiry.data.borrowing_type
+            : "Unsecured",
+          borrowing_cost: enquiry.data.collateral
+            ? enquiry.data.collateral
+            : "-",
+          credit_rating_long:
+            enquiry.data.credit_rating_long_term.length > 0
+              ? enquiry.data.credit_rating_long_term[0].rating
+              : "-",
+          credit_rating_short:
+            enquiry.data.credit_rating_short_term.length > 0
+              ? enquiry.data.credit_rating_short_term[0].rating
+              : "-",
+          current_financial_partner:
+            enquiry.data.financial_Patners.length > 0
+              ? enquiry.data.financial_Patners
+              : "-",
+          sblc_limit: enquiry.data.sblc_limit ? enquiry.data.sblc_limit : "No", // yes/no
+          lc_limit: enquiry.data.lc_limit ? enquiry.data.lc_limit : "No", // yes/no
+          supplier: enquiry.data.large_cap_supplier
+            ? enquiry.data.large_cap_supplier
+            : "No", // yes/no
+          supplier_bill_discounting: enquiry.data.supplier_discounting
+            ? enquiry.data.supplier_discounting
+            : "No", // yes/no
+          credit_amount: enquiry.data.total_current_credit_volume
+            ? enquiry.data.total_current_credit_volume
+            : "-",
+          credit_currency: enquiry.data.total_current_credit_currency
+            ? enquiry.data.total_current_credit_currency
+            : "-",
+          credit_funded_limit: enquiry.data.funded_credit
+            ? enquiry.data.funded_credit
+            : "-",
+          credit_non_funded_limit: enquiry.data.non_funded_credit
+            ? enquiry.data.non_funded_credit
+            : "-",
+          current_amount: enquiry.data.current_utilization_volume
+            ? enquiry.data.current_utilization_volume
+            : "-",
+          current_currency: enquiry.data.current_utilization_currency
+            ? enquiry.data.current_utilization_currency
+            : "-",
+          current_funded_limit: enquiry.data.funded_utilization
+            ? enquiry.data.funded_utilization
+            : "-",
+          current_non_funded_limit: enquiry.data.non_funded_utilization
+            ? enquiry.data.non_funded_utilization
+            : "-",
         },
         ignore_user_contacts: true,
         contact_infos: [
