5ee13dc650a91a7da1d0e684f24876fe01c80e0d
ChaitanyaV98 <chaitanya@betalectic.com>
2022-03-28 11:41:51 +0530

    filter fix on getallprofiles api

--- app/controllers/profile.js
+++ app/controllers/profile.js
@@ -44,6 +44,7 @@ module.exports = {
 
   async getAllProfiles(req, res) {
     try {
+      console.log("REQUEST BODY", req.user);
       let constraints = {
         where: {},
       };
@@ -53,12 +54,21 @@ module.exports = {
         constraints.where["data.country.label"] = req.body.country;
       // city: either it will be an array or a single value
       // array with array
-      if (req.body.city)
-        constraints.where["data"] = {
-          [Op.contains]: {
-            city: [{ label: req.body.city, value: req.body.city }],
-          },
-        };
+      if (req.body.type === "fm-buyer") {
+        if (req.body.city)
+          constraints.where["data"] = {
+            [Op.contains]: {
+              city: { label: req.body.city, value: req.body.city },
+            },
+          };
+      } else {
+        if (req.body.city)
+          constraints.where["data"] = {
+            [Op.contains]: {
+              city: [{ label: req.body.city, value: req.body.city }],
+            },
+          };
+      }
       if (req.body.currency)
         constraints.where["data.currency_type.value"] = req.body.currency.value;
 
