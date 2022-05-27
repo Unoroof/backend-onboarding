20577bcde097f4a0add0a702b1f8061985f16e96
maheshmuttinti <maheshmuttinti@betalectic.com>
2022-05-27 14:03:36 +0530

    #time 5m Few changes in search gm-products API

--- app/functions/getGmProducts.js
+++ app/functions/getGmProducts.js
@@ -2,7 +2,7 @@ const GmProduct = require("../models").GmProduct;
 const GmCategory = require("../models").GmCategory;
 const sequelize = require("../models").sequelize;
 
-module.exports = async function getSellersProducts(where) {
+module.exports = async function getGmProducts(where, keywordType) {
   let gmProducts = await GmProduct.findAll({
     attributes: {
       exclude: ["createdAt", "updatedAt"],
@@ -32,7 +32,7 @@ module.exports = async function getSellersProducts(where) {
   let products = JSON.parse(JSON.stringify(gmProducts));
 
   products = products.map((product) => {
-    return { ...product, keyword: "product" };
+    return { ...product, keyword: keywordType };
   });
 
   return products;
