63cd3a785eb73b5602c8e1f0633da69e79f473f9
Subbu-betalectic <subrahmanyam@betalectic.com>
2023-03-21 13:10:41 +0530

    Profile filter changes for category label and product label

--- app/loco/queries/index.js
+++ app/loco/queries/index.js
@@ -1,7 +1,11 @@
 const getProfilesByUserSubTypes = require("./getProfilesByUserSubTypes");
 const getProfilesByProductUUIDs = require("./getProfilesByProductUUIDs");
+const getProfilesByProductName = require("./getProfilesByProductName");
+const getProductUUIDsByCategoryName = require("./getProductUUIDsByCategoryName");
 
 module.exports = {
   getProfilesByUserSubTypes,
   getProfilesByProductUUIDs,
+  getProfilesByProductName,
+  getProductUUIDsByCategoryName,
 };
